4c33ab4ef694e1f63ba6b781123b102a12b499ac
Sofiia Voitkova <sofiiavoitkova@gmail.com>
2025-03-26 00:19:46 +0100

    feat: implement cart functionality

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -27,23 +27,92 @@ const CartProvider = ({ children }) => {
   }, [cart]);
 
   const addToCart = (product, id) => {
-    // your code
+    const newCart = [];
+    let found = false;
+
+    for (let i = 0; i < cart.length; i++) {
+      const item = cart[i];
+
+      if (item.id === id) {
+        found = true;
+        const updatedItem = {};
+
+        for (let key in item) {
+          updatedItem[key] = item[key];
+        }
+
+        updatedItem.amount = item.amount + 1;
+
+        newCart.push(updatedItem);
+      } else {
+        newCart.push(item);
+      }
+    }
+
+    if (!found) {
+      const newItem = {};
+      for (let key in product) {
+        newItem[key] = product[key];
+      }
+      newItem.amount = 1;
+
+      newCart.push(newItem);
+    }
+
+    setCart(newCart);
   };
 
   const removeFromCart = (id) => {
-    // your code
+    const newCart = cart.filter((item) => item.id !== id);
+    setCart(newCart);
   };
 
   const clearCart = () => {
-    // your code
+    setCart([]);
   };
 
   const increaseAmount = (id) => {
-    // your code
+    const newCart = [];
+
+    for (let i = 0; i < cart.length; i++) {
+      const item = cart[i];
+
+      if (item.id === id) {
+        const updatedItem = {};
+        for (let key in item) {
+          updatedItem[key] = item[key];
+        }
+        updatedItem.amount = item.amount + 1;
+        newCart.push(updatedItem);
+      } else {
+        newCart.push(item);
+      }
+    }
+
+    setCart(newCart);
   };
 
   const decreaseAmount = (id) => {
-    // your code
+    const newCart = [];
+
+    for (let i = 0; i < cart.length; i++) {
+      const item = cart[i];
+
+      if (item.id === id) {
+        if (item.amount > 1) {
+          const updatedItem = {};
+          for (let key in item) {
+            updatedItem[key] = item[key];
+          }
+          updatedItem.amount = item.amount - 1;
+          newCart.push(updatedItem);
+        }
+      } else {
+        newCart.push(item);
+      }
+    }
+
+    setCart(newCart);
   };
 
   return (
