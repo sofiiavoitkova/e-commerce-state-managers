f1c227d32af868f6bf3549eaee805f6b3bf51a26
Sofiia Voitkova <sofiiavoitkova@gmail.com>
2025-04-15 14:44:00 +0200

    fix: dispatch addToCart action correctly on button click

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -38,7 +38,7 @@ const ProductDetails = () => {
             </div>
             <p className="mb-8">{description}</p>
             <button
-              onClick={() => addToCart(product, product.id)}
+              onClick={() => dispatch(addToCart(product, product.id))}
               className="bg-primary py-4 px-8 text-white"
             >
               Add to cart
