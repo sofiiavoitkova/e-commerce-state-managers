c5b3e4e4ff0c8a3c5f01b0e52fc99d570322bfee
Sofiia Voitkova <sofiiavoitkova@gmail.com>
2025-04-15 17:09:10 +0200

    chore: setup store with combineReducers

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,14 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { createStore, combineReducers } from "redux";
 import cartReducer from "../reducers/cartReducer";
 import productReducer from "../reducers/productReducer";
 import sidebarReducer from "../reducers/sidebarReducer";
 
-const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    products: productReducer,
-    sidebar: sidebarReducer,
-  },
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  products: productReducer,
+  sidebar: sidebarReducer,
 });
+
+const store = createStore(rootReducer);
 
 export default store;
