c833a3181a879183aad1a83eaf56b818de616210
Sofiia Voitkova <sofiiavoitkova@gmail.com>
2025-04-17 23:10:47 +0200

    chore: remove unused files

--- src/redux/actions/productActions.js
+++
@@ -1,6 +0,0 @@
-import { SET_PRODUCTS } from "../constants/productActionTypes";
-
-export const setProducts = (products) => ({
-  type: SET_PRODUCTS,
-  payload: products,
-});
--- src/redux/selectors/productSelectors.js
+++
@@ -1,1 +0,0 @@
-export const selectAllProducts = (state) => state.products.products;
