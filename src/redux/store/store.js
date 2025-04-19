de7fa6c9f35b2bf575ac4cb321c58626ae55066c
Sofiia Voitkova <sofiiavoitkova@gmail.com>
2025-04-19 23:01:09 +0200

    chore: add redux-thunk middleware

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import cartReducer from "../reducers/cartReducer";
 import productReducer from "../reducers/productReducer";
 import sidebarReducer from "../reducers/sidebarReducer";
@@ -9,9 +10,11 @@ const rootReducer = combineReducers({
   sidebar: sidebarReducer,
 });
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
