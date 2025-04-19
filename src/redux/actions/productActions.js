22b924de347c123de6713379439080e83ff871d3
Sofiia Voitkova <sofiiavoitkova@gmail.com>
2025-04-19 23:15:56 +0200

    refactor: use async action fetchProducts instead of inline fetch

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -4,3 +4,15 @@ export const setProducts = (products) => ({
   type: SET_PRODUCTS,
   payload: products,
 });
+
+export const fetchProducts = () => {
+  return async (dispatch) => {
+    try {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      dispatch(setProducts(data));
+    } catch (err) {
+      console.error("Fetch error:", err);
+    }
+  };
+};
